783bf37fbb95fea14c91af4c00073200d911109a
MikhaelVinicius <mickaelsoel@gmail.com>
2022-06-10 03:17:03 -0300

    Continue e break

---
+++ src/Controle de fluxo/break e continue.kt
@@ -0,0 +1,22 @@
+fun main(){
+  /*  var i = 0
+    while(i < 1000000000){
+        if (i == 50){
+       break}
+
+        print(i)
+        i++
+    }
+ */
+    var i = 0
+    while(i < 80){
+        if (i < 50){
+            i++
+            continue}
+
+        print(i)
+        i++
+    }
+
+
+}
