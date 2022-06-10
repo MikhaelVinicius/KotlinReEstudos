c913ac3542d6832f2886c93ec45a77daf519232a
MikhaelVinicius <mickaelsoel@gmail.com>
2022-06-10 02:04:34 -0300

    Conceitos de for adicionados

---
+++ src/Controle de fluxo/For.kt
@@ -0,0 +1,10 @@
+fun main(){
+    for(i in 40 downTo  1 step 3){
+        print("$i ")
+    }
+    for(n in 0..10){}
+  /*  val str = "Kotlin é legal!"
+    for(char in str){
+        print("$char")
+    }
+*/}
