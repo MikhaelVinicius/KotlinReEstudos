81202093adc3249c42b5f1e61dab7cfda987ca28
MikhaelVinicius <mickaelsoel@gmail.com>
2022-06-10 03:03:48 -0300

    While e while do

---
+++ src/Controle de fluxo/doWhile.kt
@@ -0,0 +1,14 @@
+fun main(){
+    var i = 0
+    do{
+        print(i)
+        i++
+
+    }
+        while(i < 10)
+
+
+do {
+    println("Qual é o seu nome: ")
+    val value = readLine()
+} while(value == "") }
---
+++ src/Controle de fluxo/while.kt
@@ -0,0 +1,10 @@
+fun main(){
+
+    val s = "Kotlin"
+    var i = 0
+
+    while (i < s.length){
+        print(s[i])
+        i++
+    }
+}
